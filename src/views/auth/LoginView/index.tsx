d9988af3b79414da4802027f5ebe0ad963c1b76e
brye <bryewalks@gmail.com>
2019-11-16 22:44:46 -0600

    refactored loginform and signupform into one AuthForm added react router link to on login page to redirect to signup page

--- src/views/auth/LoginView/index.tsx
+++ src/views/auth/LoginView/index.tsx
@@ -1,10 +1,13 @@
-import * as React from 'react'
-import LoginForm from 'components/LoginForm'
+import React from 'react'
+import AuthForm from 'components/AuthForm'
+import { Link } from 'react-router-dom'
 
 const LoginView: React.FC = (props: any) => {
   return (
     <div>
-      <LoginForm router={props.history}/>  
+      <AuthForm signup={false}
+                router={props.history}/>
+      <Link to="/signup">Signup</Link>
     </div>
   )
 }
--- src/views/auth/SignupView/index.tsx
+++ src/views/auth/SignupView/index.tsx
@@ -1,48 +1,13 @@
-import * as React from 'react'
-import axios from 'axios';
+import React from 'react'
+import AuthForm from 'components/AuthForm'
 
 const SignupView: React.FC = (props: any) => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [passwordConfirmation, setPasswordConfirmation] = React.useState('');
-  const [name, setName] = React.useState('');
-  const [errors, setErrors] = React.useState('');
-
-  const handleSubmit = (event: any) => {
-    event.preventDefault()
-    let params = {
-      email: email,
-      password: password,
-      password_confirmation: passwordConfirmation,
-      name: name
-    };
-    axios
-      .post("/api/users", params)
-      .then(response => {
-        console.log('User Created!')
-      })
-      .catch((error) => {
-        console.log(error.response.data.errors)
-        setErrors(error.response.data.errors);
-    });
-  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input placeholder='Name'
-               onChange={e => {setName(e.target.value)}}/>
-        <input placeholder='Email'
-               type='email'
-               onChange={e => {setEmail(e.target.value)}}/>
-        <input placeholder='Password'
-               type='password'
-               onChange={e => {setPassword(e.target.value)}}/>
-        <input placeholder='Password Confirmation'
-               type='password'
-               onChange={e => {setPasswordConfirmation(e.target.value)}}/>
-        <button>Submit</button>
-      </form>
+      <AuthForm signup={true}
+                router={props.history}>
+      </AuthForm>
     </div>
   )
 }
